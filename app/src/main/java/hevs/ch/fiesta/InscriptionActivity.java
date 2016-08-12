c301b68c8ba46838f5aa0a80154bb7e639842786
ArnaudManuel <arnaud.manuel@netplus.ch>
2016-08-12 16:57:31 +0200

    test fichier de ressources

--- app/src/main/java/hevs/ch/fiesta/InscriptionActivity.java
+++ app/src/main/java/hevs/ch/fiesta/InscriptionActivity.java
@@ -3,7 +3,10 @@ package hevs.ch.fiesta;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.arnaud.myapplication.backend.service.stateWraperApi.model.StateWraper;
+
 public class InscriptionActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
