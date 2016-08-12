41ce13f803690baaa4b1a986d72d9c7337b88c86
ArnaudManuel <arnaud.manuel@netplus.ch>
2016-08-12 16:51:04 +0200

    test fichier de ressources

--- app/src/main/java/hevs/ch/fiesta/states/AppInscriptionState.java
+++ app/src/main/java/hevs/ch/fiesta/states/AppInscriptionState.java
@@ -1,10 +1,12 @@
 package hevs.ch.fiesta.states;
 
+import com.example.arnaud.myapplication.backend.userEntityApi.model.UserEntity;
+
 /**
  * Created by darle on 12.08.2016.
  */
 public class AppInscriptionState extends AppAbstractState {
-
+   UserEntity tt;
     public AppInscriptionState(){
         this.type = this.getClass().getName();
         this.validNextStep = new  String[] {
