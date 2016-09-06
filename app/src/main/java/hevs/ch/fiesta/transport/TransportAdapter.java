fd092a01560d2435539003b39aaa9acb237adb58
Yannick <yok_9@hotmail.com>
2016-09-06 07:31:35 +0200

    Test yannick transport

---
+++ app/src/main/java/hevs/ch/fiesta/transport/TransportAdapter.java
@@ -0,0 +1,21 @@
+package hevs.ch.fiesta.transport;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by Yannick on 05.09.2016.
+ */
+public class TransportAdapter extends ArrayAdapter {
+
+
+
+    public TransportAdapter(Context context, int resource, int textViewResourceId, Object[] objects) {
+        super(context, resource, textViewResourceId, objects);
+    }
+
+    // If User = conducteur
+
+    // Else affichage standard
+    
+}
---
+++ app/src/main/java/hevs/ch/fiesta/views/TranspAct.java
@@ -0,0 +1,15 @@
+package hevs.ch.fiesta.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import hevs.ch.fiesta.R;
+
+public class TranspAct extends HypermediaBrowser {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transp);
+    }
+}
